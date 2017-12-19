8b1c9205315f24a01d53911904b5ccaa11444f53
jmluczy <j.luczynski@my.ccsu.edu>
2017-12-19 17:09:30 -0600

    Add map Functions to Add and Remove Markers

--- CTFasTrakIter3/public_html/map.js
+++ CTFasTrakIter3/public_html/map.js
@@ -3,23 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var theMap;
-var centerLatLng;
 
-function initMap(){
-    map.init();
-    map.initControls();
-}
+//var map;
+
+
 
 var map = {
+    center : null,
+    visibles : [],
+    visibleKey : 0,
+    start : {
+        pos: null
+    },
+    end : {
+        pos: null
+    },
+    
     init : function(){
-        centerLatLng = new google.maps.LatLng(41.7, -72.7);
+        this.center = new google.maps.LatLng(41.7, -72.7);
         theMap = new google.maps.Map(document.getElementById('map-div'),{
             zoom: 9,
             disableDefaultUI: true,
-            center: centerLatLng,
+            center: this.center,
             mapTypeId: 'terrain'
         });
+        this.initControls();
     },
     
     initControls : function(){
@@ -31,10 +39,29 @@ var map = {
         //theMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(div);
     },
     
-    toggleUI : function(){
-        var ui = document.getElementById("user-input");
-        ui.style.display = ui.style.display === "none" ? "block" : "none";
-    }
+    addMarker : function(array){
+        //adds Marker to Map, stores reference in visibles so that they can be removed
+        var newMarker;
+        for (var i = 0; i < array.length; i++){
+            newMarker = new google.maps.Marker({
+                map: theMap,
+                position: new google.maps.LatLng(array[i].lat, arra[i].lng),
+            });
+            this.visibles.push({
+                object: newMarker,
+                key: this.visibleKey
+            });
+        }
+        visibleKey++;
+    },
+    
+    remove : function(){
+        var index = this.visibles[0].key;
+        while(this.visibles[0].key === index){
+            this.visibles[0].object.setMap(null);
+            this.visibles.shift();
+        }
+    }    
 };
 
 function toggleUI(){
